b100f586b3ef3af985f6beb0fcd3ba377c9958b4
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-03-18 12:20:07 +0700

    Connect ReactJS to Flask

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState([{}])
+
+    useEffect(() => {
+        fetch("/index").then(
+            response => response.json()
+        ).then(
+            data => {
+                setData(data)
+                console.log(data)
+            }
+        )
+    }, [])
+
+    return (
+        <div>
+            {(typeof data.name === 'undefined') ? (
+                <p>Loading...</p>
+            ) : (
+                data.name.map((name, i) => (
+                    <p key={i}>{name}</p>
+                ))
+            )}
+        </div>
+    )
 }
 
-export default App;
+export default App
--- server/server.py
+++ server/server.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/index")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return {"name": ["Manh"]}
+
+if __name__ == '__main__':
+    app.run(debug=True)
